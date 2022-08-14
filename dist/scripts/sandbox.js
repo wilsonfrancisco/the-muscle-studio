40e0b38e69c1fad7b2209ed166f04a083b265b14
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2022-08-14 21:28:58 +0100

    feat(logos and program sections done): completed the logos and the program section

--- dist/scripts/sandbox.js
+++ dist/scripts/sandbox.js
@@ -30,3 +30,17 @@ const linkAction = () => {
 };
 
 navLink.forEach((element) => element.addEventListener("click", linkAction));
+
+/**
+ * CHANGE BACKGROUND HEADER
+ */
+
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+
+  this.scrollY > 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
