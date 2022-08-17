0602c7fd013d0aedddb00d6a4443afc3f10ffecf
wilsonfrancisco <wilson.gs.francisco@gmail.com>
2022-08-17 15:46:23 +0100

    feat(scroll up): add the feature to scroll up into the page

--- dist/scripts/sandbox.js
+++ dist/scripts/sandbox.js
@@ -154,3 +154,17 @@ const scrollActive = () => {
 };
 
 window.addEventListener("scroll", scrollActive);
+
+/**
+ * SCROLL-UP
+ */
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
